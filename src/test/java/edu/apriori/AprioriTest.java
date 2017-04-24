50cec94de13c672b9a5d635a51755ba43c673ba2
jamaal.taylor <j.jerrod.taylor@gmail.com>
2017-04-24 08:23:31 -0300

    Deleting old ignore.

--- src/test/java/edu/apriori/AprioriTest.java
+++ src/test/java/edu/apriori/AprioriTest.java
@@ -1,18 +1,15 @@
 package edu.apriori;
 
-
 import edu.apriori.ItemSet;
-//import org.junit.Test;
-//import static org.junit.Assert.assertEquals;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import static org.junit.Assert.assertEquals;
 
-//import static org.junit.Assert.assertEquals;
+
 
 public class AprioriTest {
 
-    /*@Test
+    @Test
     public void testCreateItemSet(){
          ItemSet iSet = new ItemSet();
         iSet.setConfidence(.5);
@@ -41,91 +38,5 @@ public class AprioriTest {
 
         assertEquals((Integer)4, (Integer) counts.get("Bread"));
     }
-
-    @Test
-    public void testInsersection(){
-        ItemSet iSet = new ItemSet();
-        ItemSet iset2 = new ItemSet();
-
-        iSet.addItem("a");
-        iSet.addItem("b");
-        iSet.addItem("c");
-
-        iset2.addItem("a");
-        iset2.addItem("b");
-        iset2.addItem("d");
-
-        ItemSet intersection = iSet.intersection(iset2);
-
-        assertEquals(2, intersection.getItems().size());
-        assertEquals(true, intersection.getItems().contains("a"));
-    }
-
-    @Test
-    public void testSizeDifference(){
-
-        ItemSet iSet = new ItemSet();
-        ItemSet iSet2 = new ItemSet();
-
-        iSet.addItem("a");
-        iSet.addItem("b");
-        iSet.addItem("c");
-        iSet.addItem("d");
-
-        iSet2.addItem("a");
-        iSet2.addItem("b");
-
-        ItemSet diff1 = iSet.difference(iSet2);
-        ItemSet diff2 = iSet2.difference(iSet);
-        assertEquals(2, diff1.getItems().size());
-        assertEquals(0, diff2.getItems().size());
-
-        int sizeDifference = iSet.getSizeDifference(iSet2);
-        assertEquals(2, sizeDifference);
-
-        int otherSizeDifference = iSet2.getSizeDifference(iSet);
-        assertEquals(0, otherSizeDifference);
-    }
-
-    @Test
-    public void testCrossJoin(){
-        Apriori apriori = new Apriori();
-
-        ItemSet iSet = new ItemSet();
-        ItemSet iSet2 = new ItemSet();
-
-        iSet.addItem("a");
-        iSet.addItem("b");
-        iSet.addItem("c");
-
-
-        iSet2.addItem("a");
-        iSet2.addItem("b");
-        iSet.addItem("d");
-
-        ItemSet newCandidate = apriori.cross_join(iSet, iSet2);
-        assertEquals(true,newCandidate.getItems().contains("a"));
-        assertEquals(true,newCandidate.getItems().contains("b"));
-        assertEquals(true,newCandidate.getItems().contains("c"));
-        assertEquals(true,newCandidate.getItems().contains("d"));
-    }
-
-    @Test
-    public void testGenL1(){
-
-        FileHelper fileHelper = new FileHelper();
-        List<String> results = fileHelper.readLines("/Users/jamaaltaylor/Documents/filters/src/test/java/edu/uba/filters/apriori.csv");
-        Apriori apriori = new Apriori();
-        List<ItemSet> basket = apriori.create_basket(results);
-        Map<String, Integer> counts = apriori.generateCounts(basket);
-
-        List<ItemSet> largeIsets = apriori.generateLarge1ItemSets(counts,basket,.2);
-        assertEquals(5, largeIsets.size());
-    }
-
-    @Test
-    public void testAprioriGen(){
-
-    } */
 
 }
